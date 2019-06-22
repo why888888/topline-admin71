dbe1dea5a0a6cb8d6a5048543594a438b0ea782c
wanghuaiyu <15340047821@163.com>
2019-06-22 22:25:00 +0800

    创建项目及初始化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>App Component</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
