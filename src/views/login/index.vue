1e988881ad527bc6ed563ca0563aedd20e6d20bc
wanghuaiyu <15340047821@163.com>
2019-06-23 11:22:56 +0800

    用户登录-Element表单验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,11 +5,11 @@
         <img src="./logo_index.png" alt="黑马头条">
       </div>
       <div class="login-form">
-        <el-form ref='form' :model='form'>
-          <el-form-item>
+        <el-form ref='ruleForm' :model='form' :rules='rules'>
+          <el-form-item prop='mobile'>
             <el-input v-model= 'form.mobile' placeholder = '手机号'></el-input>
           </el-form-item>
-          <el-form-item>
+          <el-form-item prop='code'>
             <el-col :span='13'>
               <el-input v-model= 'form.code' placeholder = '验证码'></el-input>
             </el-col>
@@ -37,16 +37,34 @@ export default {
         mobile: '15340047821',
         code: ''
       },
+      rules: {
+        mobile: [
+          { required: true, message: '请输入后记号', trigger: 'blur' },
+          { len: 11, message: '长度必须11个', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '长度必须为6个字符', trigger: 'blur' }
+        ]
+      },
       captchaObj: null
     }
   },
   methods: {
-    handleLogin (){
+    handleLogin () {
+      this.$refs['ruleForm'].validate(valid => {
+        if (!valid) {
+          return
+        }
+        this.submitLogin()
+      })
+    },
+    submitLogin () {
       axios({
         method: 'POST',
         url: 'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
         data: this.form
-      }).then ( res => {
+      }).then(res => {
         this.$message({
           message: '登录成功',
           type: 'success'
@@ -55,7 +73,7 @@ export default {
         this.$router.push({
           name: 'home'
         })
-      }).catch (err => {
+      }).catch(err => {
         if (err.response.status === 400) {
           this.$message.error('登录失败，手机号或验证码错误')
         }
