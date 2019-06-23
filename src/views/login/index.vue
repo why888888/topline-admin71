6985ef41a5718f9483570ed746070883af50442c
wanghuaiyu <15340047821@163.com>
2019-06-23 09:13:38 +0800

    页面布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,28 @@
 <template>
-  <div>
-    <p>Login Component</p>
+  <div class="login-wrap">
+    <div class="login-form-wrap">
+      <div class="login-head">
+        <img src="./logo_index.png" alt="黑马头条">
+      </div>
+      <div class="login-form">
+        <el-form ref='form' :model='form'>
+          <el-form-item>
+            <el-input v-model= 'form.mobile' placeholder = '手机号'></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-col :span='13'>
+              <el-input v-model= 'form.mobile' placeholder = '验证码'></el-input>
+            </el-col>
+            <el-col :span='10' :offset='1'>
+              <el-button>获取验证码</el-button>
+            </el-col>
+          </el-form-item>
+          <el-form-item>
+            <el-button class="btn-login" type='primary' @click='onSubmit'>登录</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,11 +30,43 @@
 export default {
   name: 'AppLogin',
   data () {
-    return {}
+    return {
+      form: {
+        mobile: '',
+        code: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
   }
 }
 </script>
 
 <style lang="less" scoped>
-
+.login-wrap{
+  height:100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #ccc;
+  .login-head{
+    display: flex;
+    justify-content: center;
+    margin-bottom: 10px;
+    img {
+      width: 270px;
+    }
+  }
+  .login-form-wrap{
+    background-color: #fff;
+    padding:50px;
+    border-radius: 10px;
+    .btn-login{
+      width: 100%;
+    }
+  }
+}
 </style>
