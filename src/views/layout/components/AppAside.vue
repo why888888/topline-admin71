5f8820bfcf0f22486e823e1dee155c8b6f794f0d
wanghuaiyu <15340047821@163.com>
2019-06-24 11:35:42 +0800

    Layout组件-NavMenu导航菜单

--- src/views/layout/components/AppAside.vue
+++ src/views/layout/components/AppAside.vue
@@ -1,7 +1,58 @@
 <template>
-  <div>
-    <p>AppAside Component</p>
-  </div>
+  <!--
+    el-menu
+      default-active 指定默认激活的菜单项
+      @open、@close 展示和收起菜单项的时候会触发
+      background-color 设置菜单栏的背景色
+      text-color 设置菜单栏文本颜色
+      active-text-color 设置激活时候的高亮颜色
+      router 是否使用 vue-router 的模式，启用该模式会在激活导航时以 index 作为 path 进行路由跳转
+    el-submenu
+      index 导航标识，可以用作路由导航路径，必须唯一
+   -->
+  <el-menu
+    default-active="2"
+    @open="handleOpen"
+    @close="handleClose"
+    router
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b">
+    <el-menu-item index="/">
+      <i class="iconfont icon-home"></i>
+      <span slot="title">首页</span>
+    </el-menu-item>
+    <el-submenu index="1">
+      <template slot="title">
+        <i class="iconfont icon-document"></i>
+        <span>内容</span>
+      </template>
+      <el-menu-item index="/publish">
+        <i class="iconfont icon-edit"></i>
+        <span slot="title">发布</span>
+      </el-menu-item>
+      <el-menu-item index="1-3">
+        <i class="iconfont icon-icon-articles"></i>
+        <span slot="title">内容列表</span>
+      </el-menu-item>
+    </el-submenu>
+    <el-menu-item index="/comment">
+      <i class="iconfont icon-comment"></i>
+      <span slot="title">评论</span>
+    </el-menu-item>
+    <el-menu-item index="/image">
+      <i class="iconfont icon-image"></i>
+      <span slot="title">素材</span>
+    </el-menu-item>
+    <el-menu-item index="/fans">
+      <i class="iconfont icon-fensi"></i>
+      <span slot="title">粉丝</span>
+    </el-menu-item>
+    <el-menu-item index="/account">
+      <i class="iconfont icon-account"></i>
+      <span slot="title">账户</span>
+    </el-menu-item>
+  </el-menu>
 </template>
 
 <script>
@@ -14,4 +65,11 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.el-menu {
+  height: 100%;
+}
+
+.iconfont {
+  margin-right: 10px;
+}
 </style>
