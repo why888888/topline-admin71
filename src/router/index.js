7ef3e5d5747bb2aee2cc011f6468057497f55569
wanghuaiyu <15340047821@163.com>
2019-06-25 23:24:45 +0800

    创建组件并配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -27,6 +27,11 @@ const router = new Router({
           name: 'publish',
           path: '/publish',
           component: () => import('@/views/publish')
+        },
+        {
+          name: 'article-list',
+          path: '/article',
+          component: () => import('@/views/article')
         }
       ]
     }
