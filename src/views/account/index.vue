37e3f3fff42c18f52ece58c3eb7cfa240e881648
wanghuaiyu <15340047821@163.com>
2019-06-30 10:07:11 +0800

    图片收藏

---
+++ src/views/account/index.vue
@@ -0,0 +1,159 @@
+<template>
+  <el-card>
+    <div slot="header">
+      <span>账户设置</span>
+    </div>
+    <el-row>
+      <el-col :span="8">
+        <el-form>
+          <el-form-item label="媒体名称">
+            <el-input v-model="userInfo.name"></el-input>
+          </el-form-item>
+          <el-form-item label="媒体简介">
+            <el-input v-model="userInfo.intro"></el-input>
+          </el-form-item>
+          <el-form-item label="头条号ID">
+            <el-input disabled :value="userInfo.id && userInfo.id.toString()"></el-input>
+          </el-form-item>
+          <el-form-item label="绑定手机">
+            <el-input disabled :value="userInfo.mobile"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱">
+            <el-input v-model="userInfo.email"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="handleUpdate">保存更新</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+      <el-col :offset="2" :span="4">
+        <!--
+          el-upload 上传组件，他会自动将用户选择的图片请求上传，我们要做的就是配置一下
+          action 请求地址
+          由于它用的自己内部的请求，不是使用的 axios 去发请求
+              完整路径
+              它的请求也不会经过 axios 拦截器，所以需要手动配置 token
+           可惜的是它不支持自定义请求方法，前功尽弃
+        -->
+        <!-- <el-upload
+          class="avatar-uploader"
+          action="http://ttapi.research.itcast.cn/mp/v1_0/user/photo"
+          :headers="{ Authorization: token }"
+          name="photo"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload">
+          <img class="avatar" v-if="userInfo.photo" :src="userInfo.photo">
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload> -->
+        <el-upload
+          class="avatar-uploader"
+          action="http://ttapi.research.itcast.cn/mp/v1_0/user/photo"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload"
+          :http-request='handleUpload'>
+          <img class="avatar" v-if="userInfo.photo" :src="userInfo.photo">
+          <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+        </el-upload>
+      </el-col>
+    </el-row>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: 'AccountSetting',
+  data () {
+    return {
+      userInfo: {},
+      token: `Bearer ${JSON.parse(window.localStorage.getItem('user_info')).token}`
+    }
+  },
+  created () {
+    this.loadUserInfo()
+  },
+  methods: {
+    loadUserInfo () {
+      this.$http({
+        method: 'GET',
+        url: '/user/profile'
+      }).then(data => {
+        this.userInfo = data
+      })
+    },
+    handleUpdate () {
+      const { name, intro, email } = this.userInfo
+      this.$http({
+        method: 'PATCH',
+        url: '/user/profile',
+        data: {
+          name,
+          intro,
+          email
+        }
+      }).then(data => {
+        this.$store.commit('changeUser', data)
+        this.$message({
+          type: 'success',
+          message: '更新用户信息成功'
+        })
+      }).catch(err => {
+        console.log(err)
+        this.$message.error('更新用户信息失败!')
+      })
+    },
+    handleAvatarSuccess () {},
+    beforeAvatarUpload () {},
+    handleUpload (uploadConfig) {
+      // axios 上传文件
+      // 1.构建一个FormData表单对象
+      // 将文件对象添加到表单对象
+      // 2.FormData配置到请求体data选项中
+      const formData = new FormData()
+      formData.append('photo', uploadConfig.file)
+      this.$http({
+        method: 'PATCH',
+        url: '/user/photo',
+        data: formData
+      }).then(data => {
+        this.userInfo.photo = data.photo
+        this.$store.commit('changeUser', this.userInfo)
+        this.$message({
+          type: 'success',
+          message: '上传成功'
+        })
+      }).catch(err => {
+        console.log(err)
+        this.$message.error('上传失败')
+      })
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409EFF;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
---
+++ src/views/comment/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <el-card>
+    <div slot="header">
+      <span>评论管理</span>
+    </div>
+    <el-table :data="articles">
+      <el-table-column prop="title" label="标题"></el-table-column>
+      <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
+      <el-table-column prop="fans_comment_count" label="评论粉丝数"></el-table-column>
+      <el-table-column label="允许评论">
+        <template slot-scope="scope">
+          <el-switch
+            :disabled='scope.row.changeLoading'
+            v-model="scope.row.comment_status"
+            active-color="#13ce66"
+            inactive-color="#ff4949"
+            @change='handleChangeCommentStatus(scope.row)'>
+          </el-switch>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: 'ArticleComment',
+  data () {
+    return {
+      articles: []
+    }
+  },
+  created () {
+    this.loadArticles()
+  },
+  methods: {
+    loadArticles () {
+      this.$http({
+        method: 'GET',
+        url: '/articles',
+        params: {
+          response_type: 'comment'
+        }
+      }).then(data => {
+        data.results.forEach(item => {
+          item.changeLoading = false
+        })
+        this.articles = data.results
+      })
+    },
+    handleChangeCommentStatus (item) {
+      item.changeLoading = true // 禁用开关的点击状态
+      this.$http({
+        method: 'PUT',
+        url: '/comments/status',
+        params: {
+          article_id: item.id.toString()
+        },
+        data: {
+          allow_comment: item.comment_status
+        }
+      }).then(() => {
+        this.$message({
+          type: 'success',
+          message: `${item.comment_status ? '启用' : '关闭'}评论状态成功`
+        })
+        item.changeLoading = false // 启动开关的点击状态
+      }).catch(err => {
+        console.log(err)
+        item.changeLoading = false // 启用开关的点击状态
+        item.comment_status = !item.comment_status
+        this.$message.error('修改评论状态失败')
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
